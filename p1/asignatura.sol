f1b006589ec96623e5d4bc495216997f3cb9b1ae
Pablo Rubio Noguera <pabloru98@hotmail.com>
2022-10-13 17:26:47 +0200

    Comentarios eliminados

--- p1/asignatura.sol
+++ p1/asignatura.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.16;
 
 contract asignatura{
 
-    string public nombre; // Nombre de la asignatura
-    string public curso; // Curso académico
+    string public nombre;
+    string public curso;
     address public profesor;
 
     struct DatosAlumno {
@@ -17,10 +17,7 @@ contract asignatura{
         uint fecha;
         uint porcentaje;
     }
-
-    //Construye un map con clave la dirección del
-    // usuario y valor la estructura de datos que contiene
-    // los datos del alumno. Además le pongo el nombre datosAlumno.
+   
     mapping(address => DatosAlumno) public datosAlumno;
 
     Evaluacion[] public evaluaciones;
@@ -35,12 +32,7 @@ contract asignatura{
     }
 
     mapping(address => mapping(uint => Nota)) public calificaciones;
-
-    /*
-    Se especifica con memory porque estamos indicando que esos argumentos se guardan en
-    la zona de memoria que se llama memory.
-    Se ponen los nombres con "_" para que no haya conflictos con las de arriba.
-    */
+    
     constructor(string memory _nombre, string memory _curso){
         require(bytes(_nombre).length != 0, "El nombre no puede estar vacio");
         require(bytes(_curso).length != 0, "El curso no puede estar vacio");
@@ -59,8 +51,7 @@ contract asignatura{
         datosAlumno[msg.sender] = datos;
         matriculas.push(msg.sender);
     }
-
-    //El view solo dice que es una funcion de lectura, es decir, que solo lee datos
+    
     function alumnosMatriculados() public view returns(uint){
         return matriculas.length;
     }
@@ -76,12 +67,7 @@ contract asignatura{
         Evaluacion memory datos = Evaluacion(_nombre, _fecha, _porcentaje);
         evaluaciones.push(datos);        
     }
-
-    /*
-    Si asigno dentro del returns un nombre (en este caso _nombre y _email), no es necesario
-    que escriba un return como he hecho en la función automatricula.
-    Simplemente devolverá los valores a los que haya asignado a esas variables
-    */
+    
     function quienSoy() soloAlumnos public view returns(string memory _nombre, string memory _email){
         DatosAlumno memory datos = datosAlumno[msg.sender];
 
@@ -104,8 +90,7 @@ contract asignatura{
         require(msg.sender != profesor, "Solo los alumnos tienen acceso");
         _;
     }
-
-    //Toda transsacción a la que se le puede mandar dinero necesita el campo payable
+    
     receive() external payable {
         revert("Este contrato no acepta transacciones de dinero");
     }
